e5ab0d674ce1030d731e8b7f2d1bb2d2ac441a39
nikgeo01 <nborgeo@gmail.com>
2023-05-14 21:55:59 +0300

    rotating :(

---
+++ functions.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+def resize_image(image, factor):
+    size = round(image.get_width() * factor), round(image.get_height() * factor)
+    return pygame.transform.scale(image, size)
+
+def rotate_image(screen, image, top_left, angle):
+    rotated_image =  pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center = image.get_rect(topleft = top_left).center)
+    screen.blit(rotate_image, new_rect.topleft)
--- main.py
+++ main.py
@@ -2,13 +2,57 @@ import pygame
 import time
 import math
 
-TRACK = pygame.image.load("track.png")
+from functions import resize_image
+from functions import rotate_image
 
-TRACK_BORDER = pygame.image.load("track-border.jpg")
+TRACK = pygame.image.load("assets/track.png")
 
-RED_CAR = pygame.image.load("red-car.png")
-GREEN_CAR = pygame.image.load("green-car.png")
+TRACK_BORDER = pygame.image.load("assets/track-border.png")
+
+RED_CAR = resize_image(pygame.image.load("assets/red-car.png"), 0.5)
+GREEN_CAR = resize_image(pygame.image.load("assets/green-car.png"), 0.5)
 
 WIDTH = TRACK.get_width()
 HEIGHT = TRACK.get_height()
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Rider")
+
+
+class Cars:
+    IMG = RED_CAR
+
+    def __init__(self, max_vel, rotation_vel):
+        self.img = self.IMG
+        self.max_vel = max_vel
+        self.rotation_vel = rotation_vel
+        self.vel = 0
+        self.angle = 0
+
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+
+    def draw(self, screen):
+        rotate_image(screen, self.img)
+
+
+clock = pygame.time.Clock()
+while True:
+    clock.tick(60)
+    SCREEN.blit(TRACK, (0, 0))
+    SCREEN.blit(TRACK_BORDER, (0, 0))
+
+
+
+    SCREEN.blit(RED_CAR, (0, 0))
+
+
+
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
