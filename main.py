090b9b2b653b85c5d4cc68820900ce22de8c2981
nikgeo01 <nborgeo@gmail.com>
2023-05-29 00:50:55 +0300

    Winner text added. Project finished!

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ pygame.display.set_caption("Rider")
 SCORING_TIMEOUT = 840
 FONT_PATH = "assets/SHOWG.ttf"
 
+
 class Cars:
 
     def __init__(self, max_vel, rotation_vel):
@@ -65,7 +66,7 @@ class Cars:
         offset = (round(self.x - x), round(self.y - y))
         point_of_intersection = mask.overlap(car_mask, offset)
         return point_of_intersection
-    
+
     def bounce_back(self):
         self.vel = -self.vel / 2
         self.move()
@@ -76,6 +77,7 @@ class PLayerRedCar(Cars):
     y = 525
     IMG = RED_CAR
     POSITION = (x, y)
+
 
 class PlayerBlueCar(Cars):
     x = 200
@@ -104,6 +106,23 @@ def draw(screen, images, player1_car, player2_car, player1_score, player2_score,
 
     pygame.display.update()
 
+def draw_winning(screen, player1_score, player2_score, winning_font):
+    if player1_score == 3:
+        text_winning_1 = winning_font.render('RED CAR WINS!', True, (255, 255, 255))
+        textRect_winning_1 = text_winning_1.get_rect()
+        textRect_winning_1.center = (WIDTH / 2, HEIGHT / 2)
+        screen.blit(text_winning_1, textRect_winning_1)
+        pygame.display.update()
+        time.sleep(5)
+        pygame.quit()
+    if player2_score == 3:
+        text_winning_2 = winning_font.render('BLUE CAR WINS!', True, (255, 255, 255))
+        textRect_winning_2 = text_winning_2.get_rect()
+        textRect_winning_2.center = (WIDTH / 2, HEIGHT / 2)
+        screen.blit(text_winning_2, textRect_winning_2)
+        pygame.display.update()
+        time.sleep(5)
+        pygame.quit()
 
 player1_car = PLayerRedCar(4, 4)
 player2_car = PlayerBlueCar(4, 4)
@@ -116,6 +135,7 @@ player2_scoring_timeout = 0
 
 pygame.init()
 font = pygame.font.Font(FONT_PATH, 15)
+winning_font = pygame.font.Font(FONT_PATH, 50)
 
 while True:
 
@@ -151,7 +171,6 @@ while True:
     if moving2 == False:
         player2_car.slow_down()
 
-
     if player1_car.collision(TRACK_BORDER_MASK, 0, 0) != None:
         player1_car.bounce_back()
         print("Player 1 crashed")
@@ -166,13 +185,15 @@ while True:
             player1_score += 1
             print("Player 1 score: ", player1_score)
             player1_scoring_timeout = 0
-    
+
     player2_scoring_timeout += 1
     if player2_car.collision(FINISH_LINE_MASK, 250, 464) != None:
         if player2_scoring_timeout >= SCORING_TIMEOUT:
             player2_score += 1
             print("Player 2 score: ", player2_score)
             player2_scoring_timeout = 0
+
+    draw_winning(SCREEN, player1_score, player2_score, winning_font)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
